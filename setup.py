206eb02725141a3cbd44dee9c04b50ac863cd88e
nakul1234567 <nakulbhalla3@gmail.com>
2025-12-06 16:16:58 -0500

    download embedding model

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Nakul Bhalla",
+    author_email="nakulbhalla3@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src 
+mkdir -p data
+mkdir -p research
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch app.py
+touch requirements.txt
+touch research/trials.ipynb
+touch .env
+
+echo "files created"
